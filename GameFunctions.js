b180122109d64f7cd479b02e7656d581b3a71399
momoALT <momokhalid.a4@gmail.com>
2023-10-30 21:29:32 +0000

    fifteenth added working canvas when clicking play

--- GameFunctions.js
+++ GameFunctions.js
@@ -1,16 +1,21 @@
 //----------------- GAME FUNCTIONS -------------------
+
 function startGame() {
     myGameArea.start();
   }
-  
+
   var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-      window.location.href = 'game.html';
       
+      console.log('fhjsh')
         this.canvas.width = window. innerWidth;
         this.canvas.height = window. innerHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
   }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    startGame()
+}, false); 
