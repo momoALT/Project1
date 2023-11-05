99b0a86a62b5c4e680e28f4a66d0b6104660dc1c
momoALT <momokhalid.a4@gmail.com>
2023-11-05 00:29:07 +0000

    fully updated

--- JavaScript/GameFunctions.js
+++ JavaScript/GameFunctions.js
@@ -1,21 +1,38 @@
 //----------------- GAME FUNCTIONS -------------------
 var TheVoiceImage = new Image();
 var gameCanvas;
+let context;
 TheVoiceImage.src = '../Assets/Images/thevoiceface.png'
 function startGame() { //Starts game
 
   MainGameCanvas.start();
 }
+//GAME ASSETS
+let walltopHeight = window.innerHeight/2;
+let walltopWidth = window.innerWidth/4;
 
+let wallbottomHeight = window.innerHeight/2;
+let wallbottomWidth = window.innerHeight/4;
+let walltopX = window.innerHeight/3;
+let walltopY = window.innerHeight/2;
+
+let wallTop = {
+  x : walltopX,
+  y : walltopY,
+  width: walltopHeight,
+  height: walltopHeight
+}
+
+//CANVAS
 var MainGameCanvas = {
   canvas: document.createElement("canvas"), //New Canvas created
   start: function () {
     var centerimg = window.innerWidth / 2;
     
     //adding assets from images folder
-    TheVoiceImage = new component(300, 300, "../Assets/Images/thevoiceface.png", centerimg, 120, "image");
-    PerishText = new component(300, 300, "../Assets/Images/thevoiceface.png", centerimg, 120, "image");
-
+    //TheVoiceImage = new component(100, 100, "../Assets/Images/thevoiceface.png", centerimg, 100, "image");
+    
+    context = this.canvas.getContext("2d");
     gameCanvas = this.canvas
 
     gameCanvas.width = window.innerWidth;
@@ -24,6 +41,9 @@ var MainGameCanvas = {
     document.body.insertBefore(this.canvas, document.body.childNodes[0]); //Binding canvas to page
     this.frameNo = 0;
     this.interval = setInterval(updateGameArea, 20); //Setting canvas refresh time at 20ms
+    this.context.fillStyle = "red";
+    this.context.fillRect(walltopX, walltopY, walltopWidth, walltopWidth);
+
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -60,6 +80,7 @@ function component(width, height, color, x, y, type) { //Function to add assets
     this.x += this.speedX;
     this.y += this.speedY;
   }
+  
 }
 
 
@@ -76,6 +97,10 @@ document.addEventListener('DOMContentLoaded', function () {
 $("<audio id='audioElement'>").appendTo("body"); //Binds audio to body of document
 $("#audioElement").attr("src", "../Assets/Sounds/thevoicestart2.mp3").attr("autoplay", "autoplay"); //Audio Attributes with reference
 
+$("<image id='thevoiceimage'>").appendTo("body"); //Binds audio to body of document
+$("#thevoiceimage").attr("src", "../Assets/Images/thevoiceface.png"); //Audio Attributes with reference
+
+
 //Specific second of audio finder
 var triggered = false;
 var triggered2 = false;
@@ -87,7 +112,7 @@ var interval = setInterval(function () {
     triggered = true;
     $("<image id='perishID'>").appendTo("body");
     $("#perishID").attr("src", "../Assets/Images/perishtext.gif").attr("style",
-      "position: absolute; bottom: 50%; left: 50%;"
+      "position: absolute; bottom: 40%; left: 50%; transform: translateX(-50%);"
     )
   }
   if (ael.ended) clearInterval(interval);
@@ -97,6 +122,7 @@ var interval2 = setInterval(function () {
   if (!triggered2 && ael.currentTime >= 22) {// Same if as last but removes Image by second 22 of audio
     triggered2 = true;
     document.getElementById("perishID").remove();
+    document.getElementById("thevoiceimage").remove();
   }
   if (ael.ended) clearInterval(interval2);
 }, 50);
@@ -104,8 +130,14 @@ var interval2 = setInterval(function () {
 //Function to Canvas and element positions
 function updateGameArea() {
   MainGameCanvas.clear();
-  TheVoiceImage.newPos();
-  TheVoiceImage.update();
+ // TheVoiceImage.newPos();
+ // TheVoiceImage.update();
+ 
+ for (let i = 0; i < 100; i++){
+  walltopX += -0.1;
+  context.fillRect(walltopX, walltopY, walltopWidth, walltopWidth);
+ }
+
 }
 
 
